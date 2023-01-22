16c10842d2f811cd9a5bd7b40e4c46df8a4cf1f4
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-23 03:01:04 +0900

    DealerCardRepository.javaとPlayerCardRepository.javaを作成

---
+++ src/main/java/com/example/blackjack/repository/PlayerCardRepository.java
@@ -0,0 +1,54 @@
+package com.example.blackjack.repository;
+
+import com.example.blackjack.value.Card;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PlayerCardRepository {
+
+    private final JdbcTemplate jdbc;
+
+    @Autowired
+    public PlayerCardRepository(JdbcTemplate jdbcTemplate){
+        this.jdbc = jdbcTemplate;
+    }
+
+    public List<Card> readPlayerHands(){
+        // sql
+        String sql = "select card_mark, card_number " + "from player_hand ";
+
+        // queryForListメソッドでSQLを実行し、結果MapのListで受け取る。
+        List<Map<String, Object>> cards = jdbc.queryForList(sql);
+
+        // Cardオブジェクト格納用のListを作成する。
+        List<Card> cardList = new ArrayList<>();
+
+        for (Map<String, Object> eachCard : cards){
+            Card card = new Card((String) eachCard.get("card_mark"),(Integer)eachCard.get("card_number"));
+            cardList.add(card);
+        }
+        return cardList;
+    }
+
+    public void insertPlayerHands(Card card){
+        // sql
+        String sql = "insert into player_hand " + "(card_mark, card_number) " + "values(?,?)";
+        String cardMark = card.checkMark();
+        int cardNumber = card.checkNumber();
+        jdbc.update(sql, cardMark, cardNumber);
+    }
+
+    public void deletePlayerHands(){
+        // sql
+        String sql = "delete from player_hand ";
+
+        jdbc.update(sql);
+    }
+
+}
